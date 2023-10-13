491518a302124848ebb8b9ce4b27ef2b78511f45
Azur <65679301+AzurJusic@users.noreply.github.com>
2023-10-13 14:22:47 +0200

    Add files via upload

---
+++ runners/ApplicationClient.java
@@ -0,0 +1,23 @@
+package runners;
+
+
+import Services.TestService;
+
+
+
+
+public class ApplicationClient {
+
+	public static void main(String[] args) {
+		
+		TestService tst = new TestService();
+		  tst.test();
+
+	
+	} 
+	
+	
+   
+
+	
+}
